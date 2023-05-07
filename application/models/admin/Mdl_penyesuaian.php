1fee3b882a1ebd84b1b316963cb48e0577f2bcbe
cloudrider <eddy_h99@yahoo.com>
2023-05-07 20:51:22 +0800

    update penyesuaian

--- application/models/admin/Mdl_penyesuaian.php
+++ application/models/admin/Mdl_penyesuaian.php
@@ -8,15 +8,15 @@ class Mdl_penyesuaian extends CI_Model{
             
             UNION ALL
 
-            SELECT IFNULL(count(1)*-1,0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PENJUALAN_PENGUNJUNG." b ON a.id=b.id_detail WHERE jenis='items' AND id_produk=? GROUP BY id_produk;
+            SELECT IFNULL(count(1)*-1,0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PENJUALAN_PENGUNJUNG." b ON a.id=b.id_detail WHERE jenis='items' AND id_produk=? GROUP BY id_produk
             
             UNION ALL
             
-            SELECT IFNULL((count(1)*b.jumlah*-1),0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PRODUK_DETAIL." b ON a.id_produk=b.id_produk INNER JOIN ".PENJUALAN_PENGUNJUNG." c ON a.id=c.id_detail WHERE b.id_items=1 AND jenis='produk' GROUP BY b.id_items;
+            SELECT IFNULL((count(1)*b.jumlah*-1),0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PRODUK_DETAIL." b ON a.id_produk=b.id_produk INNER JOIN ".PENJUALAN_PENGUNJUNG." c ON a.id=c.id_detail WHERE b.id_items=1 AND jenis='produk' GROUP BY b.id_items
             
             UNION ALL
             
-            SELECT IFNULL(count(1)*c.jumlah*-1,0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PAKET_DETAIL." b ON a.id_produk=b.id_paket INNER JOIN ".PRODUK_DETAIL." c ON b.id_produk=c.id_produk INNER JOIN ".PENJUALAN_PENGUNJUNG." d ON a.id=d.id_detail WHERE c.id_items=1 AND jenis='paket' GROUP BY c.id_items;
+            SELECT IFNULL(count(1)*c.jumlah*-1,0) as jumlah FROM ".PENJUALAN_DETAIL." a INNER JOIN ".PAKET_DETAIL." b ON a.id_produk=b.id_paket INNER JOIN ".PRODUK_DETAIL." c ON b.id_produk=c.id_produk INNER JOIN ".PENJUALAN_PENGUNJUNG." d ON a.id=d.id_detail WHERE c.id_items=1 AND jenis='paket' GROUP BY c.id_items
             ) x";
 		$query=$this->db->query($sql,array($id,$id,$id,$id));
         return $query->row()->jumlah;
